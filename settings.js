36c4cff9a416da19aecfd69d77331f4ac3d153a5
toxxin-x <tobilobafamoroti@gmail.com>
2025-01-14 23:22:52 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,13 +6,13 @@ const chalk = require('chalk')
 
 //contact details
 global.ownernomer = "2348165846414"
-global.ownername = "𝑻𝒐𝒙𝒙𝒊𝒄 𝑩𝒐𝒚"
+global.ownername = "𝖙𝖔𝖝𝖝𝖎𝖓"
 global.ytname = "YT: ToxxicTech"
 global.socialm = "GitHub: Toxic1239"
 global.location = "Nigeria"
 
-global.ownernumber = '2348165846414'  //creator number
-global.ownername = '𝑻𝒐𝒙𝒙𝒊𝒄 𝑻𝒆𝒄𝒉' //owner name
+global.ownernumber = '2348086966448'  //creator number
+global.ownername = '𝖙𝖔𝖝𝖝𝖎𝖓' //owner name
 global.botname = '𝒁𝒆𝒏𝒐𝒏 𝒁𝒐𝒈𝒓𝒂𝒕𝒊𝒔' //name of the bot
 
 // MENU TYPE
@@ -37,7 +37,7 @@ global.themeemoji = '🤯'
 global.wm = "Toxxic Tech."
 
 //theme link
-global.link = 'https://whatsapp.com/channel/0029Vao1R2n9sBIC9sPhvI1P'
+global.link = 'https://whatsapp.com/channel/0029Vb1ufeQISTkJNkkG6T3u'
 
 //custom prefix
 global.prefa = ['','.']
